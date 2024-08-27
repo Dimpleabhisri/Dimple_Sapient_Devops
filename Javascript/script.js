1443fe44b975fb2c9a5e0cda33e3b09b35b45f29
Modadugu Dimple Abhinaya Sri <modadugu.dimpleabhinayasri@publicissapient.com>
2024-08-27 16:13:31 +0530

    Resto Application Creation

---
+++ Javascript/script.js
@@ -0,0 +1,11 @@
+function validateForm() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (name === "" || email === "" || message === "") {
+        alert("All fields must be filled out");
+        return false;
+    }
+    return true;
+}
